b27841a9cd7f893626266effaad17df54ad2b1c4
modpage1512 <pacheco1512@gmail.com>
2021-09-16 16:03:04 -0400

    listo version 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from "./pages/Home";
+import Room from "./pages/Room";
+import Error from "./pages/Error";
+import Navbar from './components/Navbar';
+import SingleRoom from './pages/SingleRoom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/rooms/" component={Room}></Route>
+        <Route  path="/rooms/:slug" component={SingleRoom}></Route>
+        <Route component={Error}></Route>
+      </Switch>
+    </>
+  )
 }
 
 export default App;
